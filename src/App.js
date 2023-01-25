5b444fde9850d1e181917ad2920804e099d2aa84
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-01-25 19:25:56 +0100

    add basic App.js structure

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
+      <nav> CV Maker </nav>
+      <main> Main </main>
+      <footer> Marek Rykaczewski </footer>
     </div>
   );
 }
