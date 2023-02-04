486835573fe789d5a36e3f9fb16d64c229567662
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-02-04 14:34:27 +0100

    create Preview component

---
+++ src/components/Preview.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class Preview extends React.Component {
+
+    render() {
+        return (
+            <div className="preview">
+                
+
+            </div>
+        )
+    }
+}
