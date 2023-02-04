218d516807b00c5556692124059b86ef29bda573
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-02-04 18:32:58 +0100

    add placeholder values to Preview

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -7,16 +7,16 @@ export default class Preview extends React.Component {
             <div className="preview">
                 <div className="preview--left">
                     <div className="preview--person"> 
-                        <div> {this.props.firstName} {this.props.lastName} </div>
+                        <div> {this.props.firstName || "FIRSTNAME"} {this.props.lastName || "LASTNAME"} </div>
                         <div> {this.props.title} </div>
                         <img className="preview--person--image" src={this.props.photo || "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png"} alt="profile" />
                     </div>
                     <div className="preview--personal-info">  
                         <div> Personal Info </div>
                         <div> Phone </div>
-                        <div> {this.props.phone} </div>
+                        <div> {this.props.phone || "PHONE-NUMBER"} </div>
                         <div> E-mail </div>
-                        <div> {this.props.email} </div>
+                        <div> {this.props.email || "E-MAIL"} </div>
                     </div>
                 </div>
 
