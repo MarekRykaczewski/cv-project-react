f105cc7aa0ba362ad45b3c1468825a074a499a60
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-02-04 18:31:21 +0100

    change first and last name concatenation

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -7,7 +7,7 @@ export default class Preview extends React.Component {
             <div className="preview">
                 <div className="preview--left">
                     <div className="preview--person"> 
-                        <div> {this.props.firstName + " " + this.props.lastName}  </div>
+                        <div> {this.props.firstName} {this.props.lastName} </div>
                         <div> {this.props.title} </div>
                         <img className="preview--person--image" src={this.props.photo || "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png"} alt="profile" />
                     </div>
