1793b034510598fdbb384af894f05a0bdd459f08
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-02-05 12:41:43 +0100

    add classes to PreviewSection

--- src/components/PreviewSection.jsx
+++ src/components/PreviewSection.jsx
@@ -6,8 +6,8 @@ export default class PreviewExperience extends React.Component {
         return (
             <div className="preview--section">
                 <div className="preview--section--title"> {Object.values(this.props.info)[1]} </div>
-                <div> {Object.values(this.props.info)[2]} {Object.values(this.props.info)[3]} </div>
-                <div> {Object.values(this.props.info)[4]} - {Object.values(this.props.info)[5]} </div>
+                <div className="preview--section--job"> {Object.values(this.props.info)[2]} {Object.values(this.props.info)[3]} </div>
+                <div className="preview--section--dates"> {Object.values(this.props.info)[4]} - {Object.values(this.props.info)[5]} </div>
             </div>
         )
     }
