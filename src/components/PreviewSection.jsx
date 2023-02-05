c5d9ed3553331335a186982355c710904627fb8e
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-02-05 12:48:39 +0100

    add comma

--- src/components/PreviewSection.jsx
+++ src/components/PreviewSection.jsx
@@ -6,7 +6,7 @@ export default class PreviewExperience extends React.Component {
         return (
             <div className="preview--section">
                 <div className="preview--section--title"> {Object.values(this.props.info)[1]} </div>
-                <div className="preview--section--job"> {Object.values(this.props.info)[2]} {Object.values(this.props.info)[3]} </div>
+                <div className="preview--section--job"> {Object.values(this.props.info)[2]}, {Object.values(this.props.info)[3]} </div>
                 <div className="preview--section--dates"> {Object.values(this.props.info)[4]} - {Object.values(this.props.info)[5]} </div>
             </div>
         )
