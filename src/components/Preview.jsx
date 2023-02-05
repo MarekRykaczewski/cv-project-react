c3b988258edfc66c47f7523df6d25c833beecbf3
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-02-05 13:50:04 +0100

    updates styling

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -22,15 +22,15 @@ export default class Preview extends React.Component {
             <div className="preview">
                 <div className="preview--left">
                     <div className="preview--person"> 
-                        <div> {general.firstName || "FIRSTNAME"} {general.lastName || "LASTNAME"} </div>
-                        <div> {general.title} </div>
+                        <div className="preview--person--name"> {general.firstName || "FIRSTNAME"} {general.lastName || "LASTNAME"} </div>
+                        <div className="preview--person--title"> {general.title || "TITLE"} </div>
                         <img className="preview--person--image" src={general.photo || "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png"} alt="profile" />
                     </div>
                     <div className="preview--personal-info">  
-                        <div> Personal Info </div>
-                        <div> Phone </div>
+                        <div className="preview--personal-info--title"> Personal Info </div>
+                        <div className="preview--personal-info--subtitle"> Phone </div>
                         <div> {general.phone || "PHONE-NUMBER"} </div>
-                        <div> E-mail </div>
+                        <div className="preview--personal-info--subtitle"> E-mail </div>
                         <div> {general.email || "E-MAIL"} </div>
                     </div>
                 </div>
