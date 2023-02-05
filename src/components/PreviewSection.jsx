f9aae61f003f3564b0b4a7347cf1ce1b5bbd1bb0
Marek Rykaczewski <m.rykaczewski99@gmail.com>
2023-02-05 12:40:49 +0100

    simplify PreviewSection html structure

--- src/components/PreviewSection.jsx
+++ src/components/PreviewSection.jsx
@@ -4,12 +4,10 @@ export default class PreviewExperience extends React.Component {
 
     render() {
         return (
-            <div>
-                <div> {Object.values(this.props.info)[1]} </div>
-                <div> {Object.values(this.props.info)[2]} </div>
-                <div> {Object.values(this.props.info)[3]} </div>
-                <div> {Object.values(this.props.info)[4]} </div>
-                <div> {Object.values(this.props.info)[5]} </div>
+            <div className="preview--section">
+                <div className="preview--section--title"> {Object.values(this.props.info)[1]} </div>
+                <div> {Object.values(this.props.info)[2]} {Object.values(this.props.info)[3]} </div>
+                <div> {Object.values(this.props.info)[4]} - {Object.values(this.props.info)[5]} </div>
             </div>
         )
     }
